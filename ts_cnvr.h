03e88a141e0f257d2119d4fbc4b5ff397ce815fa
CondeSaheki <48620486+CondeSaheki@users.noreply.github.com>
2022-01-11 22:29:49 -0300

    Update ts_cnvr.h

--- ts_cnvr.h
+++ ts_cnvr.h
@@ -7,25 +7,10 @@
 #include <chrono>
 #include <vector>
 
-#ifndef _HAS_NODISCARD
-#ifndef __has_cpp_attribute
-#define _HAS_NODISCARD 0
-#elif __has_cpp_attribute(nodiscard) >= 201603L
-#define _HAS_NODISCARD 1
-#else
-#define _HAS_NODISCARD 0
-#endif
-#endif
-#if _HAS_NODISCARD
-#define _nodiscard [[nodiscard]]
-#else
-#define _nodiscard
-#endif
-
 /* STATIC FUNCTIONS */
 
 template<typename _Elem = char, typename final_dur, typename dur>
-_nodiscard static inline constexpr final_dur func(std::basic_stringstream<_Elem>& ss) noexcept
+_NODISCARD static inline constexpr final_dur func(std::basic_stringstream<_Elem>& ss) noexcept
 {
     auto num = dur().count();
     if (!((ss) >> num))
@@ -38,7 +23,7 @@ _nodiscard static inline constexpr final_dur func(std::basic_stringstream<_Elem>
 }
 
 template<typename _Elem = char, typename final_dur, typename duration, typename duration2, typename... the_rest>
-    static inline constexpr final_dur func(std::basic_stringstream<_Elem>& ss) noexcept
+static inline constexpr final_dur func(std::basic_stringstream<_Elem>& ss) noexcept
 {
     return func<_Elem, final_dur, duration>(ss) + func<_Elem, final_dur, duration2, the_rest...>(ss);
 }
@@ -50,7 +35,7 @@ namespace ts_cnvr
     /* TS CNVR */
 
     template<typename _Elem = char, typename final_dur, typename duration, typename ...other_dur>
-        _nodiscard constexpr final_dur ts_cnvr_dur(const std::basic_string<_Elem>& str) noexcept
+    _NODISCARD constexpr final_dur ts_cnvr_dur(const std::basic_string<_Elem>& str) noexcept
     {
         std::basic_stringstream<_Elem> ss(str);
         // to do flip template parameters
@@ -60,7 +45,7 @@ namespace ts_cnvr
     }
 
     template<typename _Elem = char, typename final_dur, typename duration, typename ...other_dur>
-        void ts_cnvr_str(std::basic_string<_Elem>& str) noexcept
+    void ts_cnvr_str(std::basic_string<_Elem>& str) noexcept
     {
         std::basic_stringstream<_Elem> ss(str);
         // to do flip template parameters
@@ -75,7 +60,7 @@ namespace ts_cnvr
     /* REGEX E STR EDIT */
 
     template<typename _Elem = char>
-        void str_keep(std::basic_string<_Elem>& str, const std::basic_string<_Elem>& safe) noexcept
+    void str_keep(std::basic_string<_Elem>& str, const std::basic_string<_Elem>& safe) noexcept
     {
         bool ok;
         for (auto it = str.begin(); it != str.end(); ++it)
@@ -94,8 +79,8 @@ namespace ts_cnvr
     }
 
     template<typename _Elem = char>
-        void regex_edit(std::basic_string<_Elem>& str, const std::basic_string<_Elem>& rgx_str, 
-            void(*edit_lambda)(std::basic_string<_Elem>& str)) noexcept
+    void regex_edit(std::basic_string<_Elem>& str, const std::basic_string<_Elem>& rgx_str,
+        void(*edit_lambda)(std::basic_string<_Elem>& str)) noexcept
     {
         const std::basic_regex<_Elem> rgx(rgx_str);
         if (!str.empty() && !rgx._Empty())
@@ -114,10 +99,69 @@ namespace ts_cnvr
         }
     }
 
+    template<typename _Elem = char>
+    void regex_edit(std::basic_string<_Elem>& str, const std::basic_regex<_Elem>& rgx,
+        void(*edit_lambda)(std::basic_string<_Elem>& str)) noexcept
+    {
+        if (!str.empty() && !rgx._Empty())
+        {
+            typename std::basic_string<_Elem>::iterator match_it = str.begin();
+            std::match_results<std::basic_string<_Elem>::iterator> rgx_matchs;
+            std::basic_string<_Elem> sub_str;
+            while (std::regex_search(match_it, str.end(), rgx_matchs, rgx))
+            {
+                sub_str = rgx_matchs.str();
+                edit_lambda(sub_str);
+                match_it = get<1>((rgx_matchs.prefix())) + sub_str.size();
+                str.replace(get<1>((rgx_matchs.prefix())), get<0>((rgx_matchs.suffix())), sub_str.begin(), sub_str.end());
+                sub_str.clear();
+            }
+        }
+    }
+
+    template<typename _Elem = char>
+    _NODISCARD constexpr std::vector<std::basic_string<_Elem>> regex_results_vec(std::basic_string<_Elem>& str,
+        const std::basic_regex<_Elem>& rgx, const size_t& reverve = 10) noexcept
+    {
+        std::vector<std::basic_string<_Elem>> matches;
+        if (!str.empty() && !rgx._Empty())
+        {
+            matches.reserve(reverve);
+            typename std::basic_string<_Elem>::iterator match_it = str.begin();
+            std::match_results<std::basic_string<_Elem>::iterator> rgx_matchs;
+            while (std::regex_search(match_it, str.end(), rgx_matchs, rgx))
+            {
+                matches.emplace_back(rgx_matchs.str());
+                match_it = get<1>((rgx_matchs.prefix())) + rgx_matchs.str().size();
+            }
+        }
+        return matches;
+    }
+
+    template<typename _Elem = char>
+    _NODISCARD constexpr std::vector<std::basic_string<_Elem>> regex_results_vec(std::basic_string<_Elem>& str,
+        const std::basic_string<_Elem>& rgx_str, const size_t& reverve = 10) noexcept
+    {
+        const std::basic_regex<_Elem> rgx(rgx_str);
+        std::vector<std::basic_string<_Elem>> matches;
+        if (!str.empty() && !rgx._Empty())
+        {
+            matches.reserve(reverve);
+            typename std::basic_string<_Elem>::iterator match_it = str.begin();
+            std::match_results<std::basic_string<_Elem>::iterator> rgx_matchs;
+            while (std::regex_search(match_it, str.end(), rgx_matchs, rgx))
+            {
+                matches.emplace_back(rgx_matchs.str());
+                match_it = get<1>((rgx_matchs.prefix())) + rgx_matchs.str().size();
+            }
+        }
+        return matches;
+    }
+
     /* INPUT E OUTPUT FILE */
 
     template<typename _Elem = char>
-        _nodiscard std::basic_string<_Elem> input_file(const sfs::path& file_path) noexcept
+    _NODISCARD std::basic_string<_Elem> input_file(const sfs::path& file_path) noexcept
     {
         std::basic_string<_Elem> file;
         std::basic_ifstream<_Elem> fs;
@@ -156,7 +200,7 @@ namespace ts_cnvr
     }
 
     template<typename _Elem = char>
-        void output_file(const sfs::path& file_path, const std::basic_string<_Elem>& str) noexcept
+    void output_file(const sfs::path& file_path, const std::basic_string<_Elem>& str) noexcept
     {
         std::basic_ofstream<_Elem> fs;
         fs.open(file_path);
@@ -176,125 +220,5 @@ namespace ts_cnvr
         fs.close();
         fs.clear();
     }
+
 }
-
-/* WORKING DRAFT */
-
-#if 0
-void bom() // endianes
-{
-#if 0
-    constexpr char32_t native_bom = U'\U0000FEFF';
-
-    myifstream is("...");
-    char32_t bom;
-    is >> bom;
-    if (native_bom == bom)
-    {
-        process_stream(is);
-    }
-    else
-    {
-        mystringstream char_stream;
-        //Load the rest of `is` and endian-convert it into `char_stream`.
-        process_stream(char_stream);
-    }
-#endif
-}
-
-static std::ostream& operator<<(std::ostream& Os, const sfs::perms& perms)
-{
-    return Os << static_cast<int>(perms);
-}
-
-static bool valid_file(const sfs::path& path)
-{
-    if (sfs::exists(path) && sfs::is_regular_file(path))
-    {
-        if (sfs::is_regular_file(path))
-        {
-            // perms check
-
-
-        }
-        else
-        {
-            std::wcout << L"Error";
-            exit(1);
-        }
-    }
-    else
-    {
-        std::wcout << L"Error";
-        exit(1);
-    }
-}
-
-void perms()
-{
-    sfs::path path1(L"C:\\Users\\anonimo\\Desktop\\ARQUIVO.txt");
-    sfs::path path2(L"C:\\Users\\anonimo\\Desktop\\ARQUIVO.txt");
-
-    if (sfs::exists(path1))
-    {
-        std::wcout << L"arquivo 1 existe" << std::endl;
-    }
-    if (sfs::exists(path2))
-    {
-        std::wcout << L"arquivo 2 existe" << std::endl;
-    }
-
-    if (sfs::equivalent(path1, path2))
-    {
-        std::wcout << L"sao o mesmo arquivo" << std::endl;
-    }
-
-    if (sfs::is_regular_file(path1))
-    {
-        std::wcout << L"arquivo 1 é um arquivo regular" << std::endl;
-    }
-    if (sfs::is_regular_file(path2))
-    {
-        std::wcout << L"arquivo 2 é um arquivo regular" << std::endl;
-    }
-
-
-
-    auto perms_path1 = sfs::status(path1).permissions();
-    std::cout << perms_path1 << std::endl;
-
-    //0511
-    //0x00
-
-
-    if ((perms_path1 & sfs::perms::owner_read) != sfs::perms::none || (perms_path1 & sfs::perms::group_read) != sfs::perms::none)
-    {
-        std::wcout << L"read file -> owner or group " << std::endl;
-    }
-
-    if ((perms_path1 & sfs::perms::owner_write) != sfs::perms::none || (perms_path1 & sfs::perms::group_write) != sfs::perms::none)
-    {
-        std::wcout << L"write file -> owner or group " << std::endl;
-    }
-    if ((perms_path1 & sfs::perms::owner_exec) != sfs::perms::none || (perms_path1 & sfs::perms::group_exec) != sfs::perms::none)
-    {
-        std::wcout << L"exec file -> owner or group " << std::endl;
-    }
-
-
-
-
-    /*
-    sfs::perms::others_read;
-    sfs::perms::others_write;
-    sfs::perms::others_exec;
-    */
-
-
-    //std::wstring wstr(L"asdasdsadasd");
-    //auto size_of_text = wstr.size() * sizeof(wchar_t);
-
-    //app();
-}
-
-#endif
